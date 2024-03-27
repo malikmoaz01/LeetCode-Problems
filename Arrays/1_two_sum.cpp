bdbd4bf2145561bf2a07980083a712a555f1bff5
Malik Moaz <mlkmoaz01@gmail.com>
2024-03-27 23:27:39 +0500

    Arrays

--- Arrays/1_two_sum.cpp
+++ Arrays/1_two_sum.cpp
@@ -4,9 +4,10 @@ class Solution {
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j }; 
+                    return new int[] { i, j } ; 
                 }
             }
-        }
+        }   
+
     }
 }
